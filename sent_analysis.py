48f24911c3c1e2ffc888da949a5da3f2ad1c725d
raqoon886 <raqoon886@gmail.com>
2021-08-03 08:20:39 +0900

    initial commit

---
+++ sent_analysis.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+import pandas as pd
+from konlpy.tag import Kkma
+
+class sent_analysis:
+    
+    def __init__(self, sentence, n_gram=1, dictionary='default'):
+        
+        '''
+        args
+        sentence: a (preprocessed) sentence
+        dictionary: lexicon
+        n_gram: n_gram, default=1
+        '''
+        assert n_gram>0 , 'n_gram should be positive integer'
+        
+        kkma = Kkma()
+        
+        self.sentence = sentence
+        if dictionary=='default':
+            self.dictionary = pd.read_csv('./subjectivity-polarity.csv')
+        self.pos_set = [i+'/'+a for i,a in kkma.pos(self.sentence)]
+        if n_gram>1:
+            pos_set_ngram = []
+            for i in range(len(self.pos_set) - n_gram):
+                gram = self.pos_set[i:i+n_gram]
+                gram = ';'.join(gram)
+                pos_set_ngram.append(gram)
+            self.pos_set = pos_set_ngram
+            
+    def count_pos(self):
+        positive,neutral,negative = 0,0,0
+        for i,a in enumerate(self.pos_set):
+            temp = self.dictionary[self.dictionary['ngram']==a]
+            try:
+                positive += int(temp['POS'].values)
+                neutral += int(temp['NEUT'].values)
+                negative += int(temp['NEG'].values)
+            except:
+                pass
+
+        return {'positive':positive, 
+                'neutral':neutral, 
+                'negative':negative}
+
